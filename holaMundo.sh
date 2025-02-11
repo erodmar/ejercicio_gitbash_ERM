4b7730c335cd50479ead6dae89e440f93728b09e
erodmar <estebanrm2005@outlook.es>
2025-02-11 14:19:27 +0100

    simulacion hackeo

--- holaMundo.sh
+++ holaMundo.sh
@@ -1,1 +1,8 @@
-echo hola
+#!/bin/bash
+# Simulación de hackeo
+echo -e "\e[32mInitializing system breach...\n\e[31mUnauthorized access detected!\n\e[33mTracing IP address...\nDeploying countermeasures...\e[0m"
+sleep 3
+echo -e "\e[31mERROR: Remote server compromised. Forcing shutdown!\e[0m"
+sleep 2
+
+
